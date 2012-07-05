b61171caed043170931242f79ae0d78b55eed6f5
Vinh Tran <vinhtiensinh@yahoo.com>
2012-07-05 22:29:07 +1000

    add markuper for binding

--- lib/knockoutjs_markuper.rb
+++ lib/knockoutjs_markuper.rb
@@ -1,5 +1,16 @@
 require "knockoutjs_markuper/version"
 
-module KnockoutjsMarkuper
-  # Your code goes here...
+Markuper.builders do
+  base.after_build do |element|
+    data_bind = element.options.delete(:bind)                                           
+    all_binds = []
+
+    if data_bind.present?
+      data_bind.each_pair do |key, value|
+        all_binds.push "#{key}: #{value}"
+      end
+
+      element.options['data-bind'] = all_binds.join(', ')
+    end
+  end
 end
