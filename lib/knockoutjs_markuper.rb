2bbaf9b774dd0ea52b14d4baa13774b7be625d1b
Vinh Tran <vinhtiensinh@yahoo.com>
2012-07-14 09:33:51 +1000

    make knockout deep binding

--- lib/knockoutjs_markuper.rb
+++ lib/knockoutjs_markuper.rb
@@ -3,14 +3,22 @@ require "knockoutjs_markuper/version"
 Markuper.builders do
   base.after_build do |element|
     data_bind = element.options.delete(:bind)                                           
+    if data_bind.present?
+      element.options['data-bind'] = element._ko_bind(data_bind, true)
+    end
+  end
+
+  base.send(:define_method, :_ko_bind) do |bind_value, root=false|
     all_binds = []
 
-    if data_bind.present?
-      data_bind.each_pair do |key, value|
-        all_binds.push "#{key}: #{value}"
+    if bind_value.instance_of?(Hash)
+      bind_value.each_pair do |key, value|
+        all_binds.push "#{key}: #{self._ko_bind(value)}"
       end
-
-      element.options['data-bind'] = all_binds.join(', ')
+    else
+      return bind_value
     end
+
+    return root ? all_binds.join(', ') : "{#{all_binds.join(', ')}}"
   end
 end
