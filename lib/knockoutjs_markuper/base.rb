c8d65ec057863930865ce8066fb793a7c9c7f6bc
Vinh Tran <vinhtiensinh@yahoo.com>
2012-08-12 11:37:47 +1000

    bump up knockoutjs

--- lib/knockoutjs_markuper/base.rb
+++ lib/knockoutjs_markuper/base.rb
@@ -1,22 +1,51 @@
-Markuper.builders do
-  base.after_build do |element|
-    data_bind = element.options.delete(:bind)                                           
-    if data_bind.present?
-      element.options['data-bind'] = element._ko_bind(data_bind, true)
-    end
-  end
+module Markuper
+  class Base
+    after_build do |element|
+      data_bind = element.options.delete(:bind)                                           
 
-  base.send(:define_method, :_ko_bind) do |bind_value, root=false|
-    all_binds = []
+      element.options.each_pair do |key, value|
+        if key.to_s.match(/^bind_/)
+          element.options.delete(key)
+          binding = key.to_s.sub(/^bind_/, '')
 
-    if bind_value.instance_of?(Hash)
-      bind_value.each_pair do |key, value|
-        all_binds.push "#{key}: #{self._ko_bind(value)}"
+          data_bind = {} unless data_bind.present?
+          data_bind[binding] = value
+        end
       end
-    else
-      return bind_value
+
+      if data_bind.instance_of?(Hash)
+        attr_bindings = data_bind[:attr] || {}
+        data_bind.each_pair do |key, value|
+          if element._attribute_bindings.include? key.to_s
+            data_bind.delete(key)
+            attr_bindings[key] = value
+          end
+        end
+
+        data_bind[:attr] = attr_bindings if attr_bindings.present?
+      end
+
+      if data_bind.present?
+        element.options['data-bind'] = element._ko_bind(data_bind, true)
+      end
     end
 
-    return root ? all_binds.join(', ') : "{#{all_binds.join(', ')}}"
+    def _attribute_bindings
+      [ 'id', 'href', 'src', 'name' ]
+    end
+
+    def _ko_bind bind_value, root=false
+      all_binds = []
+
+      if bind_value.instance_of?(Hash)
+        bind_value.each_pair do |key, value|
+          all_binds.push "#{key}: #{self._ko_bind(value)}"
+        end
+      else
+        return bind_value
+      end
+
+      return root ? all_binds.join(', ') : "{#{all_binds.join(', ')}}"
+    end
   end
 end
